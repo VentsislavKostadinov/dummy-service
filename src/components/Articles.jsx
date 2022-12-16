b13027c7b2c5c43db2e0d6e1d642836c31962041
VentsislavKostadinov <ventsislav_kostadinov@abv.bg>
2022-12-16 18:02:43 +0200

    apply responsive design and loading spinner

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import "./Articles.css";
+import LoadingSpinner from "./LoadingSpinner";
 
 const Articles = () => {
     const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         retrieveData();
@@ -11,13 +13,14 @@ const Articles = () => {
 
     const retrieveData = async () => {
 
-        return fetch("https://mocki.io/v1/93ca895b-ff6e-4584-a14f-68afb735f7bd")
+        return fetch("https://mocki.io/v1/ba95adcf-c765-4b36-9ff6-5b0a7319688d")
             .then(result => {
                 return result.json();
             })
             .then(response => {
 
                 setData(response);
+                setLoading(false);
                 return response;
             })
             .catch(error => {
@@ -26,24 +29,34 @@ const Articles = () => {
     }
 
     return (
-        <div className="wrapper">
+        <div>
             {
-                data?.map((element, index) => {
-                    return (
-                        <div key={index} className="card">
-                            <img src={element.image} alt="article-image"  className="img-fluid"/>
-                            <div className="container">
-                                <h4>{element.title}</h4>
-                                <p>{element.text}</p>
-                                <div className="footer">
-                                    <span className="pipe"></span>
-                                    <span className="entertainment">{element.footer.toUpperCase()}</span>
+                loading ? (
+                    <LoadingSpinner />
+                ) : (
+                    <div className="wrapper">
+
+                        {
+                            data?.map((element, index) => {
+                                return (
+                                    <div key={index} className="card">
+                                        <img src={element.image} alt="article" className="img-fluid" />
+                                        <div className="container">
+                                            <h4>{element.title}</h4>
+                                            <p>{element.text}</p>
+                                            <div className="footer">
+                                                <span className="pipe"></span>
+                                                <span className="entertainment">{element.footer.toUpperCase()}</span>
+                                            </div>
+                                        </div>
                                     </div>
-                            </div>
-                        </div>
-                    )
-                })
+                                )
+                            })
+                        }
+                    </div>
+                )
             }
+
         </div>
     )
 }
